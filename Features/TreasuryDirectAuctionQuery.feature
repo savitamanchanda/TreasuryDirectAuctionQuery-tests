36f63b4c69b2517f9d410a18f93c562a30cbf6b9
Savita Rankavat <manchandasavita3@gmail.com>
2025-10-03 14:14:04 -0500

    Search for securities by type and date range

--- Features/TreasuryDirectAuctionQuery.feature
+++ Features/TreasuryDirectAuctionQuery.feature
@@ -8,3 +8,11 @@ Feature: Treasury Direct Securities API
     Then the response status code should be 200
     And the response body should be a non-empty JSON array
     And every security in the response array should have a "cusip" of "9128283R9"
+
+  Scenario: Search for all "Note" securities within a specific date range
+    When I make a GET request with JSON format to search for "Note" securities between "01/01/2024" and "03/31/2024"
+    Then the response status code should be 200
+    And the response body should be a non-empty JSON array
+    And every security in the response array should have a "securityType" of "Note"
+    And every security's "issueDate" should be within the requested date range
+
--- Steps/ApiSteps.cs
+++ Steps/ApiSteps.cs
@@ -1,10 +1,10 @@
-using NUnit.Framework;
-using Reqnroll;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Reqnroll;
 using TreasuryDirect.Reqnroll.Support;
-using System.Collections.Generic;
 
 namespace TreasuryDirect.Reqnroll.Steps
 {
@@ -22,14 +22,12 @@ namespace TreasuryDirect.Reqnroll.Steps
 
         //Retrieve by CUSIP
 
-        [When(@"I make a GET request with JSON format to the securities search endpoint with CUSIP ""(.*)""")]
+        [When(
+            @"I make a GET request with JSON format to the securities search endpoint with CUSIP ""(.*)"""
+        )]
         public async Task WhenIGetRequestWithCusip(string cusip)
         {
-            var query = new Dictionary<string, string>
-            {
-                { "cusip", cusip },
-                { "format", "json" }
-            };
+            var query = new Dictionary<string, string> { { "cusip", cusip }, { "format", "json" } };
             _response = await _client.CallApi(query);
         }
 
@@ -56,6 +54,41 @@ namespace TreasuryDirect.Reqnroll.Steps
             foreach (var sec in json)
             {
                 Assert.AreEqual(value, sec[field]?.ToString());
+            }
+        }
+
+        //Search for securities by type and date range
+        [When(
+            @"I make a GET request with JSON format to search for ""(.*)"" securities between ""(.*)"" and ""(.*)"""
+        )]
+        public async Task WhenISearchByTypeAndDate(string type, string startDate, string endDate)
+        {
+            var query = new Dictionary<string, string>
+            {
+                { "securityType", type },
+                { "issueDateFrom", startDate },
+                { "issueDateTo", endDate },
+                { "format", "json" },
+            };
+
+            _response = await _client.CallApi(query);
+        }
+
+        //This endpoint is returning securities out of the date range.
+        [Then(@"every security's ""(.*)"" should be within the requested date range")]
+        public async Task ThenIssueDateShouldBeWithinRange(string field)
+        {
+            var body = await _response.Content.ReadAsStringAsync();
+            var json = JArray.Parse(body);
+
+            foreach (var sec in json)
+            {
+                var issueDate = DateTime.Parse(sec[field]?.ToString());
+                Assert.IsTrue(
+                    issueDate >= DateTime.Parse("01/01/2024")
+                        && issueDate <= DateTime.Parse("03/31/2024"),
+                    $"Issue date {issueDate} is outside expected range."
+                );
             }
         }
     }
